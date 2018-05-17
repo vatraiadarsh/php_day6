2322928e5e686e726054cbdff57f9d8b3c52c362
vatraiadarsh <33037244+vatraiadarsh@users.noreply.github.com>
2018-05-17 12:32:35 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+
+#           --------------------------------------------------------------------------------
+               # Deployment in github 
+
+                # 1) git init ---> to ini the folder
+                # 2) git add . ---> current folder ko sabai vanako ho
+                # 3) git commit -m "kai message lakhna"
+                # 4) git remote add origine https://github.com/vatraiadarsh/php_day6.git(kaha push garna tasko url vayo)
+                # 5) git push -u origin master (main branch ma push garnu vano)
+                    # then it will ask the user name and password of github 
+
+                # => to add some more file in github the just do
+                    #=> git add.
+                    #=> git commit -m "any message"
+                    #=> git push -u origin master 
+#           --------------------------------------------------------------------------------
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <h1><?php echo "hello world";?></h1>
+</body>
+</html>
+
+
+
